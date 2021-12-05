e3ffd499b8e6d92d9c7ed2f6986670409d55365b
Roman Niewczas <q1cz@nykredit.dk>
2021-12-05 19:21:54 +0100

    Initial commit

---
+++ src/main/java/com/officelibrary/library/exposure/model/Book.java
@@ -0,0 +1,32 @@
+package com.officelibrary.library.exposure.model;
+
+public class Book {
+    private String title;
+    private String author;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String description;
+}

30339d9529e4e3fd8180407ff8754e0c19031527
Piotr Niewinski <Y0OF@nykredit.dk>
2022-08-22 12:26:28 +0200

    Add new service.

--- src/main/java/com/officelibrary/library/domain/BookDomain.java
+++ src/main/java/com/officelibrary/library/domain/BookDomain.java
@@ -1,12 +1,28 @@
 package com.officelibrary.library.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@Getter
-@AllArgsConstructor
 public class BookDomain {
 
     private String title;
     private AuthorDomain author;
+
+    public BookDomain(String title, AuthorDomain author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public AuthorDomain getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(AuthorDomain author) {
+        this.author = author;
+    }
 }
---
+++ src/main/java/com/officelibrary/library/service/ErrorRegistryServiceImpl.java
@@ -0,0 +1,9 @@
+package com.officelibrary.library.service;/**
+ * Place description here.
+ *
+ * @author Y0OF@nykredit.dk
+ *
+ */
+
+public class ErrorRegistryServiceImpl {
+}
